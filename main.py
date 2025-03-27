030e280ad31377f03105b82ea20ca72ea9123435
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2025-03-27 21:33:22 +1100

    🚧 parse data, add warnings for todos

--- main.py
+++ main.py
@@ -1,2 +1,41 @@
+#!/usr/bin/python
 import csv
+import sys
+import os
+import subprocess
 
+
+def usage():
+    return f'Usage: {os.path.basename(__file__)} <filename>'
+
+
+try:
+    filename = sys.argv[1]
+except IndexError:
+    sys.exit(usage())
+
+data = {}
+
+try:
+    with open(filename, 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        headings = next(reader)
+        for row in list(reader):
+            # check if data already has key headings[0]
+            if data.get(row[0]) is None:
+                try:
+                    data[row[0]] = {
+                        headings[1]: row[1].split('|'),
+                        headings[2]: row[2].split('|')
+                    }
+                    if 'TODO' in data[row[0]][headings[1]] or 'TODO' in data[row[0]][headings[2]]:
+                        print(f'⚠️ Warning: TODO(s) found in {row[0]}.')
+                except IndexError:
+                    sys.exit('The following row is incomplete: ' + str(row))
+            else:
+                sys.exit(
+                    f'"{filename}" contains more than one entry for "{headings[0]}"')
+
+except FileNotFoundError:
+    sys.exit(
+        f'"{filename}" does not exist.\n' + usage())
